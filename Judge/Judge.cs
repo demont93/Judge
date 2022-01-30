3849978a79b27fc35d4c2acd066abc750ade9eb2
Derek Montesdeoca <demont93@gmail.com>
2022-01-30 14:32:59 +0100

    Initial functionality outline for JudgeCore.

--- Judge/Judge.cs
+++ Judge/Judge.cs
@@ -4,24 +4,45 @@ namespace Shinpan
 {
     public class Judge
     {
-        private readonly ProblemInfo _problem;
+        private readonly IProblemInfo _problemInfo;
+        private Process _process;
+        private StreamWriter _output;
 
-        public Judge (ProblemInfo problemInfo, Script script, Language language)
+        public Judge(IProblemInfo problemInfo, Process process, StreamWriter output)
         {
-
-            //Language lang = DetectLanguage();
-            //lang.RunScript(_problem.gameSet);
+            _problemInfo = problemInfo;
+            _process = process;
+            _output = output;
         }
 
-        /*
-        private Language DetectLanguage() 
+        public void Run()
         {
-            return Path.GetExtension(this._script.FilePath) switch
+            // Todo: Allow for async processing of testcases.
+            foreach (var testCaseIo in _problemInfo.TestCases())
             {
-                //"lisp" => new CPP(),
-                "cc" or "cpp" => new CPP(),
-                _ => throw new Exception("Language compiling not implemented"),
-            };
-        }*/
+                // Fixme: Can this process be reused?
+                _process.Start();
+                var start = _process.StartTime;
+
+                // Writes the Inputs from TestFile into the Input of the Solution.
+                _process.StandardInput.WriteLine(testCaseIo.Input);
+
+                // Writes the Output from Solution into the output file.
+                // Todo: add StdErr to output when ExitCode != 0.
+                _output.Write(_process.StandardOutput.ReadToEnd());
+                _process.WaitForExit();
+
+                _output.Flush();
+
+                // Fixme: Not used.
+                var timeElapsed = (_process.ExitTime - start).Milliseconds;
+                
+                // Todo:
+                // Store run metrics.
+                // Check if constraints are met.
+                // Display run metrics.
+                // Display if solution is correct.
+            }
+        }
     }
 }
